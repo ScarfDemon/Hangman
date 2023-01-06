9571795f32af9cd6a08ce52f2eacb177b32913ed
Afia Saeed <Afia@Afias-MacBook-Pro.local>
2023-01-06 22:18:58 +0000

    Task 2- random.choice

--- milestone_2.py
+++ milestone_2.py
@@ -1,5 +1,11 @@
 # %%
+import random
+
+# %% # Task 1- make word list
 word_list = ['Loquat', 'Banana', 'Mango', 'Persimmon', 'Lychee']
 print(word_list)
 
+# %% # Task 2- use random choice
+word = random.choice(word_list)
+print(word)
 # %%
