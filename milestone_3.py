3bf3bb012660b308c25b1e188a7efc6f020d263f
Afia Saeed <Afia@Afias-MacBook-Pro.local>
2023-01-08 17:30:35 +0000

    Added and used list most common words; M3 Task 2- Check whether the guess is in the word

--- milestone_3.py
+++ milestone_3.py
@@ -1,3 +1,8 @@
+# %%
+import random
+word_list = [i for i in list((open('1-1000.txt','r').read()).split("\n")) if len(i)>2]
+word = random.choice(word_list)
+print(word)
 # %% #Task 1- iteratively check if input is a valid guess
 while True:
     guess = input("Please guess a letter: ")
@@ -8,3 +13,15 @@ while True:
         continue
 
 # %%
+
+if guess.lower() in word.lower():
+    print(f"Good guess! {guess} is in the word.")
+    guess = input("Please enter another letter: ") 
+else:
+    print(f"Sorry, {guess} is not in the word. Try again.")
+    guess = input("Please enter a letter: ")  
+# %%
+
+
+# %%
+
