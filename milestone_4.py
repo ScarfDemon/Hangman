743e427377c4365059f8efec56da7ed76c6e23a0
Afia Saeed <Afia@Afias-MacBook-Pro.local>
2023-01-13 18:53:49 +0000

    M4 Task 2- create methods for running checks- check_guess, ask_for_input

--- milestone_4.py
+++ milestone_4.py
@@ -1,6 +1,8 @@
+# %%
 import random
 word_list = [i for i in list((open('1-1000.txt','r').read()).split("\n")) if len(i)>2]
 
+# %%
 
 class Hangman:
     def __init__(self, word_list, num_lives = 5):
@@ -10,29 +12,35 @@ class Hangman:
         self.unique_letters = set([i for i in self.word])
         self.word_guessed = ['_']*len(self.word)
         self.list_of_guesses = []
-        # self.guess = guess
-        # for i in range(len(self.word)-1):
-        #     if self.guess == self.word[i]:
-        #         self.word[i] = self.guess
-        #         self.unique_letters.remove(self.guess)
         self.num_letters = len(set(self.word))
     
-    # def check_guess(guess):
-    #     guess = guess.lower()
-    #     if guess in word:
-    #         print(f"Good guess! {guess} is in the word.")
-    #         guess = input("Please enter another letter: ") 
-    #     else:
-    #         print(f"Sorry, {guess} is not in the word. Try again.")
-    #         guess = input("Please enter a letter: ")  
+    def check_guess(self, guess):
+        guess = guess.lower()
+        if guess in self.word:
+            print(f"Good guess! {guess} is in the word.")
+            #guess = input("Please enter another letter: ") 
+        else:
+            print(f"Sorry, {guess} is not in the word. Try again.")
+            #guess = input("Please enter a letter: ")  
 
-    # def ask_for_input():
-    #     while True:
-    #         guess = input("Please guess a letter: ")
-    #         if (len(guess) == 1) and guess.isalpha():
-    #             break
-    #         else:
-    #             print("Invalid letter. Please, enter a single alphabetical character.")
-    #             continue
-    #     check_guess(guess)
+    def ask_for_input(self):
+        while True:
+            guess = input("Please guess a letter: ")
+            if not ((len(guess) == 1) and guess.isalpha()):
+                print("Invalid letter. Please, enter a single alphabetical character.")
+                continue
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter!")
+                continue
+            else:
+                self.check_guess(guess)
+
+        #         for i in range(len(self.word)-1):
+        #             if self.guess == self.word[i]:
+        #                 self.word[i] = self.guess
+        #                 self.unique_letters.remove(self.guess)
+        # 
+
+# %%
+Hangman(word_list).ask_for_input()
 
